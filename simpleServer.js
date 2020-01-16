f65703ed48bef166b29c5187ead691ef95f2dc37
Zaporozhets Anton <zaporozhetsanton@gmail.com>
2020-01-16 10:46:08 +0200

    add response for new intent

--- simpleServer.js
+++ simpleServer.js
@@ -26,25 +26,42 @@ app.get('/', (req, res) => res.send('online'))
 app.post('/dialogflow', express.json(), (req, res) => {
   const agent = new WebhookClient({ request: req, response: res })
 
-  async function getPersonInfo () {
+  const addCharacterToDb = newCharacter => {
+    console.log(newCharacter)
+    console.log('in this function Kosh will add new person to DB')
+  }
+
+  const getCharacterInfo = async () => {
     const requestedName = req.body.queryResult.parameters.person.name
     const personFromDB = fakeDB.find(person => person.name.toLowerCase().includes(requestedName.toLowerCase()))
     if (personFromDB) {
-      return fetch(personFromDB.url)
-        .then(res => res.text())
-        .then(body => {
-          const personFromServer = JSON.parse(body)
-          const personInfo = personFromServer.gender === 'male'
-            ? `He is ${personFromServer.gender}, his height is ${personFromServer.height}, his skin color is ${personFromServer.skin_color}, his eyes are ${personFromServer.eye_color}.`
-            : `She is ${personFromServer.gender}, her height is ${personFromServer.height}, her skin color is ${personFromServer.skin_color}, her eyes are ${personFromServer.eye_color}.`
-          agent.add(personInfo)
-        })
+      const response = await fetch(personFromDB.url)
+      const character = await response.text()
+      const personFromServer = JSON.parse(character)
+      const personInfo = personFromServer.gender === 'male'
+        ? `He is ${personFromServer.gender}, his height is ${personFromServer.height}, his skin color is ${personFromServer.skin_color}, his eyes are ${personFromServer.eye_color}.`
+        : `She is ${personFromServer.gender}, her height is ${personFromServer.height}, her skin color is ${personFromServer.skin_color}, her eyes are ${personFromServer.eye_color}.`
+      agent.add(personInfo)
+      return
     }
     agent.add(`There is no star wars person with name ${requestedName}, you can try to find somebody with another name or you can add new character to the database.`)
   }
 
-  let intentMap = new Map()
-  intentMap.set('star wars name', getPersonInfo)
+  const addNewCharacter = () => {
+    const newCharacter = {
+      name: req.body.queryResult.parameters.person.name,
+      height: req.body.queryResult.parameters.height,
+      gender: req.body.queryResult.parameters.gender,
+      eye_color: req.body.queryResult.parameters.eye_color,
+      skin_color: req.body.queryResult.parameters.skin_color,
+    }
+    addCharacterToDb(newCharacter)
+    agent.add('New person was created and added to DB')
+  }
+
+  const intentMap = new Map()
+  intentMap.set('star wars character request', getCharacterInfo)
+  intentMap.set('star wars add character', addNewCharacter)
   agent.handleRequest(intentMap)
 })
 
